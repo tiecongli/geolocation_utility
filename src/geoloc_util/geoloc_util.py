a0ae9227260db18162b8f2aa12240d2fc11c4292
Tiecong Li <tiecongli@Tiecongs-Mac-mini.local>
2025-04-06 02:50:44 -0700

    use httpx and async

--- src/geoloc_util/geoloc_util.py
+++ src/geoloc_util/geoloc_util.py
@@ -1,51 +1,91 @@
+import random
 from typing import Dict, List
-import requests
+from typing_extensions import override
+import httpx
+import asyncio
 import logging
+import sys
+
+class JitterRetryTransport(httpx.AsyncHTTPTransport):
+    def __init__(self, retries=3, backoff_factor=1.0, jitter_max=1.0, status_forcelist={429, 500, 502, 503, 504}, **kwargs):
+        super().__init__()
+        self._retries = retries
+        self._backoff_factor = backoff_factor
+        self._jitter_max = jitter_max
+        self._status_forcelist = status_forcelist
+
+    @override
+    async def handle_async_request(self, request: httpx.Request) -> httpx.Response:
+        for attempt in range(self._retries + 1):
+            try:
+                response = await super().handle_async_request(request)
+                if response.status_code in self._status_forcelist:
+                    raise httpx.HTTPStatusError(
+                        message=f"Received retryable status: {response.status_code}",
+                        request=request,
+                        response=response
+                    )
+                return response
+            except (httpx.RequestError, httpx.HTTPStatusError) as e:
+                if attempt == self._retries:
+                    raise e
+                jitter = random.uniform(0, self._jitter_max)
+                sleep_time = self._backoff_factor * (2 ** attempt) + jitter
+                logging.info(f"{e}, retry {attempt + 1} after {sleep_time}")
+                await asyncio.sleep(sleep_time)
+
 
 class GeoLocator:
     API_KEY = "f897a99d971b5eef57be6fafa0d83239"
     BASE_URL = "http://api.openweathermap.org/geo/1.0"
     LIMIT = 5
+    TIMEOUT = 5.0
 
     @classmethod
-    def fetch_geolocation(cls, locations: List[str]) -> List[Dict[str, any]]:
+    async def fetch_geolocation(cls, locations: List[str]) -> List[Dict[str, any]]:
         results = []
+        transport = JitterRetryTransport(retries=3, backoff_factor=1.0, jitter_max=1.0, status_forcelist={429, 500, 502, 503, 504})
         for location in locations:
             if location.isdigit():  # zip case
                 url = f"{cls.BASE_URL}/zip?zip={location},US&appid={cls.API_KEY}"
             else:  # place zipcode combo
                 url = f"{cls.BASE_URL}/direct?q={location},US&limit={cls.LIMIT}&appid={cls.API_KEY}"
 
-            logging.info(f"Making request to {url}")   
-            response = requests.get(url)
-            response.raise_for_status()
-            data = response.json()
-            logging.info(f"Respond: {data}")
+            logging.info(f"Making request to {url}")
+            async with httpx.AsyncClient(timeout = cls.TIMEOUT, transport=transport) as client:
+                try:
+                    response = await client.get(url)
+                    response.raise_for_status()
+                    data = response.json()
+                    logging.info(f"Respond: {data}")
             
-            if data and isinstance(data, list):  # For place/state combo
-                data = data[0] # Return the first coordination per requirement
-                results.append({
-                    "name": data.get("name"),
-                    "latitude": data["lat"],
-                    "longitude": data["lon"],
-                    "country": data.get("country"),
-                    "state": data.get("state")
-                })
-            elif data and isinstance(data, dict):
-                results.append({
-                    "zip": data["zip"],
-                    "name": data.get("name"),
-                    "lat": data.get("lat"),
-                    "lon": data["lon"],
-                    "country": data.get("country")
-                })
-            else:
-                results.append({
-                    "result": f"Geo location info not found for {location}"
-                })
+                    if data and isinstance(data, list):  # For place/state combo
+                        data = data[0] # Return the first coordination per requirement
+                        results.append({
+                            "name": data.get("name"),
+                            "latitude": data["lat"],
+                            "longitude": data["lon"],
+                            "country": data.get("country"),
+                            "state": data.get("state")
+                        })
+                    elif data and isinstance(data, dict):
+                        results.append({
+                            "zip": data["zip"],
+                            "name": data.get("name"),
+                            "lat": data.get("lat"),
+                            "lon": data["lon"],
+                            "country": data.get("country")
+                        })
+                    else:
+                        results.append({
+                            "result": f"Geo location info not found for {location}"
+                        })
+                except (httpx.HTTPStatusError, httpx.RequestError) as e:
+                    logging.error(f"Error: {e}")
         return results
 
-if __name__ == "__main__":
+
+def main():
     import argparse
     import re
 
@@ -54,6 +94,13 @@ if __name__ == "__main__":
     parser.add_argument("pos_loc", nargs="*", help="List of locations (e.g., 'Madison, WI', '12345').")
     parser.add_argument("--log", action="store_true", help="Enable logging")
     args = parser.parse_args()
+
+    if args.locations:
+        locations = args.locations
+    elif args.pos_loc:
+        locations = args.pos_loc
+    else:
+        parser.error("Locations are needed either via --locations or as position arguments") # this will raise SystemExit(2)
 
     def validate_prepare_locations(locations: List[str]) -> List[str]:
         regex4PlaceStateCombo = r"^([a-zA-Z\s]+),\s*([a-zA-Z]{2})\s*$"
@@ -68,14 +115,6 @@ if __name__ == "__main__":
                 raise ValueError(f"Invalid location format: {location}")
         
         return preparedLocations
-            
-
-    if args.locations:
-        locations = args.locations
-    elif args.pos_loc:
-        locations = args.pos_loc
-    else:
-        parser.error("Locations are needed either via --locations or as position arguments")
 
     if args.log:
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -84,9 +123,15 @@ if __name__ == "__main__":
 
     locations = validate_prepare_locations(locations=locations)
 
+    results = asyncio.run(GeoLocator.fetch_geolocation(locations=locations))
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
     try:
-        results = GeoLocator.fetch_geolocation(locations=locations)
-        for result in results:
-            print(result)
-    except Exception as e:
+        main()        
+    except Exception as e: # Not catch SystemExit because it inherits from BaseExeption, not Exception
+        logging.info(f"Error: {e}")
         print(f"Error: {e}")
+        sys.exit(1)
--- tests/test_geoloc_util.py
+++ tests/test_geoloc_util.py
@@ -1,57 +1,83 @@
 import pytest
-import requests
+import logging
 import subprocess
 import os
 import ast
 from geoloc_util.geoloc_util import GeoLocator
 
-def test_happypath_place_state(test_data):
-    actual = GeoLocator.fetch_geolocation([test_data["data1"]["city_state"]])
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_happypath_place_state(test_data):
+    actual = await GeoLocator.fetch_geolocation([test_data["data1"]["city_state"]])
     expected = [test_data["data1"]["geo_location_city_state"]]
     assert actual == expected
 
-def test_happypath_zip(test_data):
-    actual = GeoLocator.fetch_geolocation([test_data["data1"]["zipcode"]])
+
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_happypath_zip(test_data):
+    actual = await GeoLocator.fetch_geolocation([test_data["data1"]["zipcode"]])
     expected = [test_data["data1"]["geo_location_zip"]]
     assert actual == expected 
 
-def test_happypath_multiple_place_state(test_data):
-    actual = GeoLocator.fetch_geolocation([test_data["data1"]["city_state"], test_data["data2"]["city_state"]])
+
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_happypath_multiple_place_state(test_data):
+    actual = await GeoLocator.fetch_geolocation([test_data["data1"]["city_state"], test_data["data2"]["city_state"]])
     expected = [test_data["data1"]["geo_location_city_state"], test_data["data2"]["geo_location_city_state"]]
     assert actual == expected 
 
-def test_happypath_multiple_zip(test_data):
-    actual = GeoLocator.fetch_geolocation([test_data["data1"]["zipcode"], test_data["data2"]["zipcode"]])
+
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_happypath_multiple_zip(test_data):
+    actual = await GeoLocator.fetch_geolocation([test_data["data1"]["zipcode"], test_data["data2"]["zipcode"]])
     expected = [test_data["data1"]["geo_location_zip"], test_data["data2"]["geo_location_zip"]]
     assert actual == expected 
 
-def test_happypath_mix_place_state_zip(test_data):
-    actual = GeoLocator.fetch_geolocation([test_data["data1"]["city_state"], test_data["data2"]["zipcode"]])
+
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_happypath_mix_place_state_zip(test_data):
+    actual = await GeoLocator.fetch_geolocation([test_data["data1"]["city_state"], test_data["data2"]["zipcode"]])
     expected = [test_data["data1"]["geo_location_city_state"], test_data["data2"]["geo_location_zip"]]
     assert actual == expected 
 
-def test_invalid_place_state():
-    actual = GeoLocator.fetch_geolocation(["Santa Clara, wa"])
+
+@pytest.mark.integ
+@pytest.mark.asyncio
+async def test_invalid_place_state():
+    actual = await GeoLocator.fetch_geolocation(["Santa Clara, wa"])
     expected = [{"result": "Geo location info not found for Santa Clara, wa"}]
     assert actual == expected
     
-def test_invalid_zip():
-    with pytest.raises(requests.exceptions.HTTPError) as ex_info:
-        GeoLocator.fetch_geolocation(["99999"])    
-    assert "Not Found" in str(ex_info.value)
 
+@pytest.mark.integ
+@pytest.mark.asyncio  
+async def test_invalid_zip(caplog):
+    with caplog.at_level(logging.INFO):
+        await GeoLocator.fetch_geolocation(["99999"])    
+    assert "Not Found" in caplog.text
+
+
+@pytest.mark.e2e
 def test_e2e_happypath_place_state(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, test_data["data1"]["city_state"]], capture_output=True, text=True)
     assert result.returncode == 0
     assert ast.literal_eval(result.stdout.replace("\n", "")) == test_data["data1"]["geo_location_city_state"]
 
+
+@pytest.mark.e2e
 def test_e2e_happypath_zip(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, test_data["data1"]["zipcode"]], capture_output=True, text=True)
     assert result.returncode == 0
     assert ast.literal_eval(result.stdout.replace("\n", "")) == test_data["data1"]["geo_location_zip"]
 
+
+@pytest.mark.e2e
 def test_e2e_happypath_multi_place_state(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, test_data["data1"]["city_state"], test_data["data2"]["city_state"]], capture_output=True, text=True)
@@ -59,7 +85,9 @@ def test_e2e_happypath_multi_place_state(test_data, app_path):
     results = result.stdout.split("\n")
     assert ast.literal_eval(results[0]) == test_data["data1"]["geo_location_city_state"]
     assert ast.literal_eval(results[1]) == test_data["data2"]["geo_location_city_state"]
-    
+
+
+@pytest.mark.e2e
 def test_e2e_happypath_multi_zip(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, test_data["data1"]["zipcode"], test_data["data2"]["zipcode"]], capture_output=True, text=True)
@@ -68,6 +96,8 @@ def test_e2e_happypath_multi_zip(test_data, app_path):
     assert ast.literal_eval(results[0]) == test_data["data1"]["geo_location_zip"]
     assert ast.literal_eval(results[1]) == test_data["data2"]["geo_location_zip"]
 
+
+@pytest.mark.e2e
 def test_e2e_happypath_multi_mix(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, test_data["data1"]["zipcode"], test_data["data2"]["city_state"]], capture_output=True, text=True)
@@ -76,6 +106,8 @@ def test_e2e_happypath_multi_mix(test_data, app_path):
     assert ast.literal_eval(results[0]) == test_data["data1"]["geo_location_zip"]
     assert ast.literal_eval(results[1]) == test_data["data2"]["geo_location_city_state"]
 
+
+@pytest.mark.e2e
 def test_e2e_happypath_multi_mix_with_locations_arg(test_data, app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, "--locations", test_data["data1"]["zipcode"], test_data["data2"]["city_state"]], capture_output=True, text=True)
@@ -84,36 +116,48 @@ def test_e2e_happypath_multi_mix_with_locations_arg(test_data, app_path):
     assert ast.literal_eval(results[0]) == test_data["data1"]["geo_location_zip"]
     assert ast.literal_eval(results[1]) == test_data["data2"]["geo_location_city_state"]
 
+
+@pytest.mark.e2e
 def test_e2e_missing_args(app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app], capture_output=True, text=True)
     assert result.returncode == 2
     assert "Locations are needed either via --locations or as position arguments" in result.stderr
 
+
+@pytest.mark.e2e
 def test_e2e_invalid_args_format(app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, "--locations", "San Luis Obispo,,"], capture_output=True, text=True)
     assert result.returncode == 1
-    assert "Invalid location format" in result.stderr
+    assert "Invalid location format" in result.stdout
 
+
+@pytest.mark.e2e
 def test_e2e_invalid_args_state_format(app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, "--locations", "San Luis Obispo,CAA"], capture_output=True, text=True)
     assert result.returncode == 1
-    assert "Invalid location format" in result.stderr
+    assert "Invalid location format" in result.stdout
 
+
+@pytest.mark.e2e
 def test_e2e_invalid_args_zip_format(app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, "--locations", "89054945894"], capture_output=True, text=True)
     assert result.returncode == 1
-    assert "Invalid location format" in result.stderr
+    assert "Invalid location format" in result.stdout
 
+
+@pytest.mark.e2e
 def test_e2e_zip_not_found(app_path):
     app = os.path.abspath(app_path)
-    result = subprocess.run([app, "--locations", "99999"], capture_output=True, text=True)
+    result = subprocess.run([app, "--log", "--locations", "99999"], capture_output=True, text=True)
     assert result.returncode == 0
-    assert "Not Found" in result.stdout
+    assert "Not Found" in result.stderr
 
+
+@pytest.mark.e2e
 def test_e2e_zip_city_state_not_found(app_path):
     app = os.path.abspath(app_path)
     result = subprocess.run([app, "--locations", "Shanghai, tx"], capture_output=True, text=True)
